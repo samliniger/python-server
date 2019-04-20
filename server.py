e12350ae84e59c470cf4cb1c68e32dde388de558
samliniger <samuel.liniger@uzh.ch>
2019-04-20 21:29:55 +0200

    Make request limit configurable

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
+#Server directory web for multiple requests
 import http.server
 import socketserver
 import os
 
+try:
+  limit=int(os.environ['REQUEST_LIMIT'])
+except:
+  limit=0
 PORT = 8000
 
 web_dir = os.path.join(os.path.dirname(__file__), 'web')
@@ -10,4 +15,13 @@ os.chdir(web_dir)
 Handler = http.server.SimpleHTTPRequestHandler
 httpd = socketserver.TCPServer(("", PORT), Handler)
 print("serving at port", PORT)
-httpd.serve_forever()
+
+i = 1
+
+if limit > 0:
+  while i < limit:
+    httpd.handle_request()
+    i += 1
+  httpd.socket.close()
+else:
+  httpd.serve_forever()
